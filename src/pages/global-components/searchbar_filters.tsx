c7c9a76c50d3aa1479f08a92db6ae5d93e1e7f29
aadesh-cisit <aadesh.kumar@cisit.in>
2025-05-02 18:12:38 +0530

    searchbar removed

--- src/pages/global-components/searchbar_filters.tsx
+++ src/pages/global-components/searchbar_filters.tsx
@@ -133,10 +133,7 @@ export default function SearchbarFilters({ onFiltersChange }: SearchbarFiltersPr
         <div className="flex flex-col space-y-6 ">
             <div className="max-w-xl" id="container">
                 <div className="flex flex-row justify-between items-center gap-4">
-                    <div className="relative flex-1 max-w-md">
-                        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                        <Input className="pl-10" placeholder="Search..." />
-                    </div>
+                 
 
                     <Dialog open={isModalOpen} onOpenChange={setIsModalOpen} >
                         <DialogTrigger asChild>
